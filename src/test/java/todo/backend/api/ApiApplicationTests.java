5a5b36825a25f3aeaa0d5595bc02b4187070eb65
ntando <ntando.sghp@gmail.com>
2026-03-08 12:21:51 -0800

    First Commit

---
+++ src/test/java/todo/backend/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
