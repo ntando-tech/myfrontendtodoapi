6a578c6f6e81e8c5567f80a7c58bf1dd7355f477
ntando <ntando.sghp@gmail.com>
2026-04-17 10:42:03 -0800

    Edited & Moved complete checkbox & Removed Config for RestClient

---
+++ src/main/java/todo/frontend/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package todo.frontend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/todo/frontend/api/model/Task.java
@@ -0,0 +1,54 @@
+package todo.backend.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Setter
+@Getter
+//@NoArgsConstructor
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+
+    private String description;
+
+    //@Enumerated(EnumType.STRING)
+    private String priority;
+
+    private LocalDate created;
+
+    private String dueDate;
+
+    private boolean completed;
+
+
+    public Task(String title, String description, String priority,
+                String dueDate) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.created = LocalDate.now();
+        this.completed = false;
+        this.dueDate = dueDate;
+
+    }
+
+    public Task(){}
+
+
+
+}
