7236b4459019540f740092e5dfcacb31dc081d21
ntando <ntando.sghp@gmail.com>
2026-04-17 12:03:15 -0800

    recovered configuration folder for RestClient

--- src/main/java/todo/backend/api/controller/PageController.java
+++ src/main/java/todo/backend/api/controller/PageController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import todo.backend.api.model.Task;
-
+import todo.backend.api.config.RestClientConfig;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,13 +18,14 @@ public class PageController {
     private final String API_URL = "http://localhost:8080/api/tasks";
 
     private final RestTemplate restTemplate;
+
     public PageController(RestTemplate restTemplate){
         this.restTemplate = restTemplate;
-    }
+   }
     @GetMapping("/todos")
     public String getAllTasks(Model model) {
 
-        RestTemplate restTemplate = new RestTemplate();
+       // RestTemplate restTemplate = new RestTemplate();
 
         Task[] tasksArray = restTemplate.getForObject(API_URL, Task[].class);
         List<Task> tasks = Arrays.asList(tasksArray);
@@ -37,7 +38,7 @@ public class PageController {
     @GetMapping("/todo/{id}")
     public String viewSingleTask(@PathVariable Long id, Model model) {
 
-        RestTemplate restTemplate = new RestTemplate();
+       // RestTemplate restTemplate = new RestTemplate();
 
         //Task[] taskArray = restTemplate.getForObject( API_URL+"/" + id, Task[].class);
         Task task = restTemplate.getForObject( API_URL+"/todo/{id}", Task.class,id);
@@ -56,7 +57,7 @@ public class PageController {
     @PostMapping("/todo/save/task")
     public String saveTask(@ModelAttribute Task task) {
 
-        RestTemplate restTemplate = new RestTemplate();
+       // RestTemplate restTemplate = new RestTemplate();
 
         restTemplate.postForObject(API_URL, task, Task.class);
 
@@ -65,14 +66,14 @@ public class PageController {
 
     @PutMapping("/todo/{id}")
     public String updateTask(@PathVariable Long id, @ModelAttribute Task task){
-        RestTemplate restTemplate = new RestTemplate();
+        //RestTemplate restTemplate = new RestTemplate();
         restTemplate.put(API_URL+"/todo/{id}",task,id);
         return "redirect:/todos";
     }
 
       @GetMapping("/completedTasks")
       public String getCompletedTasks(Model model){
-
+            //RestTemplate restTemplate = new RestTemplate();
          Task[] completedTasks = restTemplate.getForObject(API_URL, Task[].class);
          List<Task> completedTaskss = Arrays.asList(completedTasks);
          model.addAttribute("tasks", completedTaskss);
@@ -89,8 +90,9 @@ public class PageController {
     @DeleteMapping("/todo/{id}")
     public String deleteTask(@PathVariable Long id) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.delete(API_URL + "/todo/{id}", id);
+       //RestTemplate restTemplate = new RestTemplate();
+        restTemplate.delete(API_URL + "/todo/"+id);
+
 
         return "redirect:/todos";
     }
--- src/test/java/todo/backend/ApiApplicationTests.java
+++ src/test/java/todo/backend/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package todo.backend.api;
+package todo.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
