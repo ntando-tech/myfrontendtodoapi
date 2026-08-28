247f36ef116af7d484146bbee1f72b72d4055ba1
ntando <ntando.sghp@gmail.com>
2026-08-28 13:16:41 -0800

    Uncommented createNotification on signup

--- src/main/java/todo/backend/api/controller/PageController.java
+++ src/main/java/todo/backend/api/controller/PageController.java
@@ -84,12 +84,12 @@ public class PageController {
         try {
             model.addAttribute("forgotPasswordMessage", "");
 //            System.out.println("Starting to store notification");
-            Notification notification = new Notification("Forgot password", "Link to reset password was sent");
+  //          Notification notification = new Notification("Forgot password", "Link to reset password was sent");
 
-            System.out.println("Here's my title: "+notification.getTitle());
-            System.out.println("Here's my description: "+ notification.getDescription());
-            System.out.println("Ended to store notification");
-            restTemplate.postForEntity(NOTIFICATION_API_URL+"/testcreatenotification", notification, Void.class);
+  //          System.out.println("Here's my title: "+notification.getTitle());
+  //          System.out.println("Here's my description: "+ notification.getDescription());
+  //          System.out.println("Ended to store notification");
+           // restTemplate.postForEntity(NOTIFICATION_API_URL+"/testcreatenotification", notification, Void.class);
             String message = String.valueOf(restTemplate.postForObject(NORMAL_API_URL+"/forgotpassword", users, String.class));
             System.out.println("Message:" + message);
 
