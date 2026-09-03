6e7081fb1437e6e576bad9d1ec7f5c78fd1c8b57
ntando <ntando.sghp@gmail.com>
2026-09-02 18:24:28 -0800

    Commented createNotification code on signup

--- src/main/java/todo/backend/api/controller/PageController.java
+++ src/main/java/todo/backend/api/controller/PageController.java
@@ -44,11 +44,11 @@ public class PageController {
     public String signup(@ModelAttribute Users users, Model model) {
         try {
             System.out.println("Before sending data to backend");
-            Notification notification = new Notification("New Account", "Welcome to Task Tracker");
-            notification.setUser(users);
-            System.out.println("Here's my title: "+notification.getTitle());
-            System.out.println("Here's my description: "+ notification.getDescription());
-            System.out.println("Ended to store notification");
+//            Notification notification = new Notification("New Account", "Welcome to Task Tracker");
+//            notification.setUser(users);
+//            System.out.println("Here's my title: "+notification.getTitle());
+//            System.out.println("Here's my description: "+ notification.getDescription());
+//            System.out.println("Ended to store notification");
 
             String message = String.valueOf(restTemplate.postForObject(NORMAL_API_URL+"/register", users, String.class));
             System.out.println("Starting if statements");
@@ -59,7 +59,7 @@ public class PageController {
                 model.addAttribute("showPasswordMessage", "Email already exists");
                 return "signup";
             } else if (message.equals("User was added successfully")) {
-                restTemplate.postForEntity(NOTIFICATION_API_URL+"/testcreatenotification", notification, Void.class);
+              //  restTemplate.postForEntity(NOTIFICATION_API_URL+"/testcreatenotification", notification, Void.class);
                 return "redirect:/signin";
             } else {
                 System.out.println("This is else statement");
